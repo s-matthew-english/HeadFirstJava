6c97c2c8b9ed9f891b915cb3256c66b3d6184399
s.matthew.english <s.matthew.english@gmail.com>
2018-03-31 14:13:05 -0400

    Add comments

--- MiniMiniMusicApp/MiniMiniMusicApp.java
+++ MiniMiniMusicApp/MiniMiniMusicApp.java
@@ -1,3 +1,4 @@
+// Don't forget to import the midi package.
 import javax.sound.midi.*;
 
 public class MiniMiniMusicApp {
@@ -10,13 +11,29 @@ public class MiniMiniMusicApp {
   public void play() {
     try {
 
+     /**
+      * Get a Sequencer and open it (so we can use it... a Sequencer doesn't come already open).
+      */
       Sequencer player = MidiSystem.getSequencer();
       player.open();
 
+     /**
+      * Don't worry about the arguments to the Sequence constructor. 
+      */
       Sequence seq = new Sequence(Sequence.PPQ, 4);
 
+
+     /**
+      * Ask the Sequencer for a Track. Remember, the Track lives in the Sequence, and the MIDI
+      * data lives in the Track.
+      */
       Track track = seq.createTrack();
 
+     /**
+      * Put some MIDIEvents into the Track. This part is mostly "Ready-bake" code. The only thing 
+      * you'll have to care about are the arguments to the setMessage() method, and the arguments
+      * to the MIDIEvent constructor. We'll look at those arguments on the next page.
+      */
       ShortMessage a = new ShortMessage();
       a.setMessage(144, 1, 44, 100);
       MidiEvent noteOn = new MidiEvent(a, 1);
@@ -27,8 +44,14 @@ public class MiniMiniMusicApp {
       MidiEvent noteOff = new MidiEvent(b, 16);
       track.add(noteOff);
 
+     /**
+      * Give the Sequence to the Sequencer (like putting the CD in the CD player). 
+      */
       player.setSequence(seq);
 
+     /**
+      * Invoke start() on the Sequencer (like pushing PLAY).
+      */
       player.start();
     } catch (Exception ex) {
       ex.printStackTrace();
